1d15d2149abd37ac02e4222058631873fd93d44a
Joachim <joachimjahr@hotmail.com>
2017-03-28 23:00:46 +0200

    encrypt added

--- help_method_test.py
+++ help_method_test.py
@@ -1,16 +1,8 @@
 import unittest
 import help_methods as HM
-from Crypto.Cipher import AES  # pip pycrypto
-import base64
-AES_key = "This_is_the_test_key. Not_very_secret"
+AES_key = "This_is_the_test_key.very_secret"
+print(len(AES_key))
 
-
-def encrypt(word):
-    iv = 16 * '\x00' #init vector
-    obj = AES.new(AES_key, AES.MODE_CBC, iv)
-    data = obj.encrypt(base64.b64decode(word))
-    print(data)
-    return data
 
 class TestHelpMethods(unittest.TestCase):
     """Tests functions in help_method files"""
@@ -20,8 +12,7 @@ class TestHelpMethods(unittest.TestCase):
         passwords = ["A", "Hei", "Vary", "Looong password, with_some|chars", "I guess this is enough?",
                      "Ill throw in a few extra weird chars", "@£$€{[]}@£$§!'\"#¤%&/()=?", "1234567890\¨'*^`=´~\\"]
         for password in passwords:
-            encrypted = encrypt(password)
-            # TODO: @Halvor. encrypt varying length and format passwords, and decrypt them with decrypt
+            encrypted = HM.encrypt(password)
             decrypted = HM.decrypt(encrypted)
             self.assertEqual(password, decrypted)
 
