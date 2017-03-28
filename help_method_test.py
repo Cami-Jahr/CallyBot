9a43545d7cea08cb8a14dddad86f90c17f6f01d1
Joachim <joachimjahr@hotmail.com>
2017-03-28 22:26:31 +0200

    Slight changes

--- help_method_test.py
+++ help_method_test.py
@@ -1,9 +1,20 @@
 import unittest
 import help_methods as HM
+from Crypto.Cipher import AES  # pip pycrypto
+import base64
+AES_key = "This_is_the_test_key. Not_very_secret"
 
+
+def encrypt(word):
+    iv = 16 * '\x00' #init vector
+    obj = AES.new(AES_key, AES.MODE_CBC, iv)
+    data = obj.encrypt(base64.b64decode(word))
+    print(data)
+    return data
 
 class TestHelpMethods(unittest.TestCase):
     """Tests functions in help_method files"""
+
     def test_decrypt(self):
         """Tests decrypt and remove_padding"""
         passwords = ["A", "Hei", "Vary", "Looong password, with_some|chars", "I guess this is enough?",
