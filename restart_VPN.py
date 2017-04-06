a75da61bf460fcd195fce67f2182f95c0a7fd975
TFFJJ <joachimjahr@hotmail.com>
2017-04-06 15:12:53 +0200

    Added information strings

--- restart_VPN.py
+++ restart_VPN.py
@@ -7,9 +7,10 @@ pw = help_methods.decrypt(credentials.Credentials().feide[1])
 
 
 def restart_vpn():  # Server screen is 1366x768
+    """Assume no program is above server, and icons in correct position"""
     pyautogui.moveTo(1201, 748)  # The location of the VPN in the toolbar
     pyautogui.click()
-    pyautogui.moveTo(1289, 641)
+    pyautogui.moveTo(1289, 641)  # Location of connect button
     pyautogui.click()
     time.sleep(6)
     pyautogui.click()
