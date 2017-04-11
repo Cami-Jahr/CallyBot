8674b87de05591ad5e88a2e70fcf06e4b5207f07
TFFJJ <joachimjahr@hotmail.com>
2017-04-11 20:23:38 +0200

    Updated restart VPN with higher sleep times, to better fit slow computer

--- restart_VPN.py
+++ restart_VPN.py
@@ -1,20 +1,24 @@
 import pyautogui
-import time
-import credentials
-import help_methods
+from time import sleep
+from credentials import Credentials
+from help_methods import decrypt
 
-pw = help_methods.decrypt(credentials.Credentials().feide[1])
+pw = decrypt(Credentials().feide[1])
 
 
 def restart_vpn():  # Server screen is 1366x768
-    """Assume no program is above server, and icons in correct position"""
+    """Assume no program is above server, and icons in correct position
+    All timings can and should be adapted to servers speed. Cordinates must be changed to screen resolution
+    """
     pyautogui.moveTo(1201, 748)  # The location of the VPN in the toolbar
+    sleep(5)
     pyautogui.click()
     pyautogui.moveTo(1289, 641)  # Location of connect button
+    sleep(5)
     pyautogui.click()
-    time.sleep(6)
+    sleep(15)
     pyautogui.click()
-    time.sleep(6)  # Wait of login to open
+    sleep(15)  # Wait of login to open
     pyautogui.typewrite(pw)
     pyautogui.press('enter')
-    time.sleep(12)
+    sleep(15)
